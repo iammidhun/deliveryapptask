a60b76f3e99b285e389f44fa37eab054c5e39df2
iammidhun <midhunmohanputhur@gmail.com>
2021-02-06 19:09:04 +0530

    order details model

--- app/models/orderdetailmodel.py
+++ app/models/orderdetailmodel.py
@@ -1,1 +1,7 @@
 from app.app import db
+from app.models.usermodel import User
+
+class OrderDetails(db.Document):
+	itemname = db.StringField(required=True)
+	pickupaddress = db.StringField(required=True)
+	ordereduser = db.ReferenceField(User)
