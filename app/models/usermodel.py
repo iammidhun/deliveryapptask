fc61396155684d51ae3ec344b038393b17651dc9
iammidhun <midhunmohanputhur@gmail.com>
2021-02-06 18:18:44 +0530

    usermodel defined for user and deliveryuser

--- app/models/usermodel.py
+++ app/models/usermodel.py
@@ -1,1 +1,10 @@
+#for user and delivery user databse
 from app.app import db
+
+class User(db.Document):
+	username = db.StringFiled(required=True)
+	usermailid = db.StringFiled(required=True)
+	userrole = db.StringFiled(required=Truddddddde)
+	userpassword = db.StringFiled(required=True)
+	deliveryaddress = db.StringFiled()
+	userphoneno = db.StringFiled(required=True)
