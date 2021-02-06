bfa658ce87937100f7443972cbc1b917fa8a8bac
iammidhun <midhunmohanputhur@gmail.com>
2021-02-06 18:05:18 +0530

    mongodb settings defined in app.py , imported MongoEngine

--- app/app.py
+++ app/app.py
@@ -1,5 +1,11 @@
 from flask import Flask
+from flask_mongoengine import MongoEngine
 from flask_restful import  Api
 app = Flask(__name__)
+app.config["MONGODB_SETTINGS"] = {
+	"db":"deliveryapp",
+	"host":"localhost",
+	"port":27017
+}
 app.secret_key = b'_5#y2L"Fjnmb\n\xec]/'
 api = Api(app)
