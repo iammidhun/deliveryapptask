3af13fc0385893a2b4ba19976f3cc0e72a3c0975
iammidhun <midhunmohanputhur@gmail.com>
2021-02-07 09:38:27 +0530

    user registration fileds defined

--- app/views/Userregistration.py
+++ app/views/Userregistration.py
@@ -4,3 +4,11 @@ from flask_restful import Resource
 from flask import jsonify,request,render_template
 def userlogin():
 	return render_template('login.html')
+
+def useregister():
+	username = request.form["username"]
+	userpassword = request.form["userpassword"]
+	usermailid = request.form["usermailid"]
+	userrole = request.form["userrole"]
+	deliveryaddress = request.form["deliveryaddress"]
+	userphoneno = request.form["userphoneno"]
