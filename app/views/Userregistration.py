bfe76cb7ab128b9889c22be6cccaa1f2b3ed0219
iammidhun <midhunmohanputhur@gmail.com>
2021-02-07 09:27:48 +0530

    user login defined

--- app/views/Userregistration.py
+++ app/views/Userregistration.py
@@ -2,5 +2,5 @@
 from app.models.usermodel import User
 from flask_restful import Resource
 from flask import jsonify,request,render_template
-def userregister():
+def userlogin():
 	return render_template('login.html')
