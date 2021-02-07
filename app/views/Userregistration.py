f9ee7c6dad7f5631691e58ce2000c017376fa365
iammidhun <midhunmohanputhur@gmail.com>
2021-02-07 11:20:32 +0530

    render template added

--- app/views/Userregistration.py
+++ app/views/Userregistration.py
@@ -1,18 +1,22 @@
 #for user signup
 from app.models.usermodel import User
 from flask_restful import Resource
-from flask import jsonify,request,render_template
+from flask import jsonify,request,render_template,redirect, url_for
 def userlogin():
 	return render_template('login.html')
 
 def userregister():
-	username = request.form["username"]
-	userpassword = request.form["userpassword"]
-	usermailid = request.form["usermailid"]
-	userrole = request.form["userrole"]
-	deliveryaddress = request.form["deliveryaddress"]
-	userphoneno = request.form["userphoneno"]
-	User.objects.create(username=username,userpassword=userpassword
-		usermailid=usermailid,userrole=userrole,
-		deliveryaddress=deliveryaddress,userphoneno=userphoneno)
-	return ("success")
+	if request.method == 'POST':
+		username = request.form["username"]
+		userpassword = request.form["userpassword"]
+		usermailid = request.form["usermailid"]
+		userrole = request.form["userrole"]
+		deliveryaddress = request.form["deliveryaddress"]
+		userphoneno = request.form["userphoneno"]
+		print(request.form)
+		User.objects.create(username=username,userpassword=userpassword,
+			usermailid=usermailid,userrole=userrole,
+			deliveryaddress=deliveryaddress,userphoneno=userphoneno)
+		return redirect(url_for('userlogin'))
+	else:
+		return render_template('register.html')
