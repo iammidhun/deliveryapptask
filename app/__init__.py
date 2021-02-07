79e449a154524ed54af7e4584fabf249d9b884b5
iammidhun <midhunmohanputhur@gmail.com>
2021-02-07 23:54:56 +0530

    router for delivery agent added

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from app.app import api, app, login_manager
 from app.models.usermodel import User
 from app.views.Userregistration import userregister,userlogin
-from app.views.orderitem import orderitem
+from app.views.orderitem import orderitem,orderupdate
 login_manager.init_app(app)
 @login_manager.user_loader
 def load_user(user):
@@ -10,3 +10,4 @@ def load_user(user):
 app.add_url_rule("/", view_func=userlogin, methods=["GET","POST"])
 app.add_url_rule("/register",view_func=userregister,methods=["GET","POST"])
 app.add_url_rule("/orderitem",view_func=orderitem,methods=["GET","POST"])
+app.add_url_rule("/orderupdate",view_func=orderupdate,methods=["GET","POST"])
