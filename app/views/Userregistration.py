e7c1021217c2a44fb7c29b5d4e3bf6aa6296a2a4
iammidhun <midhunmohanputhur@gmail.com>
2021-02-08 00:47:39 +0530

    bug fixed

--- app/views/Userregistration.py
+++ app/views/Userregistration.py
@@ -4,13 +4,14 @@ from app.app import login_manager
 from app.models.usermodel import User
 from flask_restful import Resource
 from app.views.orderitem import orderitem
-from flask import jsonify,request,render_template,redirect, url_for
+from flask import jsonify,request,flash,render_template,redirect, url_for
 from flask_login import current_user,LoginManager,login_required,login_user,logout_user
 def userlogin():
 	if request.method == 'POST':
 		user = User.objects.get(usermailid=request.form["usermailid"])
 		if (user.userpassword==request.form["password"]):
 			login_user(user)
+			flash('You were successfully logged in')
 			return redirect(url_for('orderitem'))
 		else:
 			render_template('login.html')
