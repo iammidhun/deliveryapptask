0c3a4645e174b4b8946134ed666e620edfd530f3
iammidhun <midhunmohanputhur@gmail.com>
2021-02-07 11:19:55 +0530

    get method added in user register function

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
 from app.app import api, app
 from app.views.Userregistration import userregister,userlogin
 app.add_url_rule("/", view_func=userlogin, methods=["GET","POST"])
-app.add_url_rule("/register",view_func=userregister,methods=["POST"])
+app.add_url_rule("/register",view_func=userregister,methods=["GET","POST"])
