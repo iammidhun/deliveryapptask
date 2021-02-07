a4a73fe63dfd453a98692e1f7730584758b8fe5f
iammidhun <midhunmohanputhur@gmail.com>
2021-02-07 13:09:08 +0530

    user model defined

--- app/models/usermodel.py
+++ app/models/usermodel.py
@@ -8,3 +8,27 @@ class User(db.Document):
 	userpassword = db.StringField(required=True)
 	deliveryaddress = db.StringField()
 	userphoneno = db.StringField(required=True)
+
+	def is_authenticated(self):
+		return True
+
+	def get_id(self):
+		return str(self.id)
+
+	def is_active(self):
+		return True
+
+	def is_anonymous(self):
+		return False
+
+	def get_role(self):
+		return self.role
+
+	def check_role(self, roles):
+		return self.role in roles
+
+	# meta = {
+	# 	'allow_inheritance': True,
+	# 	'indexes': ['-date_created', 'email'],
+	# 	'ordering': ['-date_created']
+	# }
