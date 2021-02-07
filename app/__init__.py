4a2ad0b527f894787171413b6ba9a03c29c05ba0
iammidhun <midhunmohanputhur@gmail.com>
2021-02-07 13:08:29 +0530

    login manager defined

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,10 @@
-from app.app import api, app
+from app.app import api, app, login_manager
+from app.models.usermodel import User
 from app.views.Userregistration import userregister,userlogin
+login_manager.init_app(app)
+@login_manager.user_loader
+def load_user(user):
+    """current user details"""
+    return User.objects.get(id=user)
 app.add_url_rule("/", view_func=userlogin, methods=["GET","POST"])
 app.add_url_rule("/register",view_func=userregister,methods=["GET","POST"])
--- app/app.py
+++ app/app.py
@@ -1,6 +1,10 @@
 from flask import Flask
 from flask_mongoengine import MongoEngine
+from flask_login import LoginManager
 from flask_restful import  Api
+from flask_login import current_user
+from flask_login import LoginManager
+login_manager = LoginManager()
 app = Flask(__name__,template_folder='../templates')
 app.config["MONGODB_SETTINGS"] = {
 	"db":"deliveryapp",
