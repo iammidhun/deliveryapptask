5b0074cd8bfdb93ccdfd7213364ac9e5e6511299
iammidhun <midhunmohanputhur@gmail.com>
2021-02-07 15:03:28 +0530

    updating user details

--- app/views/orderitem.py
+++ app/views/orderitem.py
@@ -1,2 +1,19 @@
 from flask import jsonify,request,render_template,redirect, url_for
 from flask_login import current_user
+from app.models.usermodel import User
+from app.models.orderdetailmodel import OrderDetails
+def orderitem():
+	if request.method == 'POST':
+		itemname = request.form["itemname"]
+		pickupaddress = request.form["pickupaddress"]
+		deliveryaddresstype = request.form["pickupaddress"]
+		deliveryaddress = request.form["deliveryaddress"]
+		if  deliveryaddresstype == "new":
+			user =  User.objects.get(id=current_user.id)
+			deliveryaddresslist = user.deliveryaddress
+			deliveryaddresslist.append(deliveryaddress)
+			user.deliveryaddress = deliveryaddresslist
+			user.save()
+		
+	
+
