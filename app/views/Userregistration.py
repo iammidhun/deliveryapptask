36154c8b4de34602ac4ee7ec2556e9eb6656d6f4
iammidhun <midhunmohanputhur@gmail.com>
2021-02-07 13:11:00 +0530

    login added

--- app/views/Userregistration.py
+++ app/views/Userregistration.py
@@ -1,8 +1,17 @@
 #for user signup
+from app.app import login_manager
 from app.models.usermodel import User
 from flask_restful import Resource
 from flask import jsonify,request,render_template,redirect, url_for
+from flask_login import current_user,LoginManager,login_required,login_user,logout_user
 def userlogin():
+	if request.method == 'POST':
+		user = User.objects.get(usermailid=request.form["usermailid"])
+		if (user.userpassword==request.form["password"]):
+			login_user(user)
+			print(current_user.username)
+		else:
+			render_template('login.html')
 	return render_template('login.html')
 
 def userregister():
@@ -20,3 +29,4 @@ def userregister():
 		return redirect(url_for('userlogin'))
 	else:
 		return render_template('register.html')
+def logout()
