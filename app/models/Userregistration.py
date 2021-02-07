a20ebfbb28924878fb6d16098c4c94ef262bf203
iammidhun <midhunmohanputhur@gmail.com>
2021-02-07 09:06:08 +0530

    removed from model

--- app/models/Userregistration.py
+++
@@ -1,6 +0,0 @@
-#for user signup
-from app.models.usermodel import User
-from flask_restful import Resource
-from flask import jsonify,request,render_template
-def userregister():
-	return render_template('login.html')
